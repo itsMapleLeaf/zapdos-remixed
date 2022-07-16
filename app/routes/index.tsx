fdab4f211a0c7ca5c0119582d98e488b157d9ec3
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-07-16 12:40:37 -0500

    twitch auth

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,33 @@
+import type { LoaderArgs } from "@remix-run/node"
+import { json } from "@remix-run/node"
+import { Form, useLoaderData } from "@remix-run/react"
+import { authenticator } from "~/auth/auth.server"
+
+export async function loader({ request }: LoaderArgs) {
+  const user = await authenticator.isAuthenticated(request)
+  // eslint-disable-next-line unicorn/no-null
+  return json({ user: user ? { name: user.displayName } : null })
+}
+
 export default function Index() {
+  const { user } = useLoaderData<typeof loader>()
   return (
-    <main className="">
-      <h1>the</h1>
+    <main>
+      <header>
+        <h1>zapdos remixed</h1>
+        {user ? (
+          <>
+            <p>hi there, {user.name}!</p>
+            <Form method="post" action="/auth/logout">
+              <button type="submit">Sign out</button>
+            </Form>
+          </>
+        ) : (
+          <Form method="post" action="/auth/twitch/login">
+            <button type="submit">Sign in with Twitch</button>
+          </Form>
+        )}
+      </header>
     </main>
   )
 }
