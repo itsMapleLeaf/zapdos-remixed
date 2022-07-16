b52635e0427efff9d081fb3792055665f8b6bf85
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-07-16 14:01:47 -0500

    list questions

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,25 +2,50 @@ import type { LoaderArgs } from "@remix-run/node"
 import { json } from "@remix-run/node"
 import { Form, useLoaderData } from "@remix-run/react"
 import { authenticator } from "~/auth.server"
+import { db } from "~/db.server"
 
 export async function loader({ request }: LoaderArgs) {
-  const user = await authenticator.isAuthenticated(request)
-  // eslint-disable-next-line unicorn/no-null
-  return json({ user: user ? { name: user.displayName } : null })
+  const streamer = await authenticator.isAuthenticated(request)
+  if (!streamer) {
+    return json({ streamer: undefined, questions: undefined })
+  }
+
+  const questions = await db.question.findMany({
+    where: {
+      streamerId: streamer.id,
+    },
+  })
+
+  return json({
+    streamer: {
+      name: streamer.displayName,
+    },
+    questions: questions.map((question) => ({
+      id: question.id,
+      text: question.text,
+    })),
+  })
 }
 
 export default function Index() {
-  const { user } = useLoaderData<typeof loader>()
+  const data = useLoaderData<typeof loader>()
   return (
     <main>
       <header>
         <h1>zapdos remixed</h1>
-        {user ? (
+        {"streamer" in data ? (
           <>
-            <p>hi there, {user.name}!</p>
+            <p>hi there, {data.streamer.name}!</p>
             <Form method="post" action="/auth/logout">
               <button type="submit">Sign out</button>
             </Form>
+            <ul>
+              {data.questions.map((question) => (
+                <li key={question.id}>
+                  <p>{question.text}</p>
+                </li>
+              ))}
+            </ul>
           </>
         ) : (
           <Form method="post" action="/auth/twitch/login">
