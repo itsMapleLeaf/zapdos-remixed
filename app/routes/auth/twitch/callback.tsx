e80a5971c648e6117ac84cf938c3f1b259c1c889
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-07-16 14:00:54 -0500

    show auth errors

--- app/routes/auth/twitch/callback.tsx
+++ app/routes/auth/twitch/callback.tsx
@@ -1,9 +1,5 @@
 import type { LoaderFunction } from "@remix-run/node"
 import { authenticator } from "~/auth.server"
 
-export const loader: LoaderFunction = ({ request }) => {
-  return authenticator.authenticate("twitch", request, {
-    successRedirect: "/",
-    failureRedirect: "/",
-  })
-}
+export const loader: LoaderFunction = ({ request }) =>
+  authenticator.authenticate("twitch", request, { successRedirect: "/" })
