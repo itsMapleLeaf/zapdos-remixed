cc43327e524d4101eea1948a765af65f9ce4999e
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-07-16 21:52:54 -0500

    add a page on auth callback to show catch boundary

--- app/routes/auth/twitch/callback.tsx
+++ app/routes/auth/twitch/callback.tsx
@@ -3,3 +3,9 @@ import { authenticator } from "~/auth.server"
 
 export const loader: LoaderFunction = ({ request }) =>
   authenticator.authenticate("twitch", request, { successRedirect: "/" })
+
+// even though this should technically never show,
+// rendering a page will show the catch boundary if auth fails
+export default function Page() {
+  return <p>success!</p>
+}
